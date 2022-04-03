91657ac2f4a0ac97dff0118adb681699ea0294b5
MF <Frotan1954@gamil.com>
2022-04-03 10:58:52 +0430

    this is SevenClass Version

--- src/SevenClass.java
+++ src/SevenClass.java
@@ -1,6 +1,10 @@
-public static void main(String[] args) {
+import java.util.Arrays;
+
+public class SevenClass {
+    public static void main(String[] args) {
 
         String s=" i liked ti move it move it";
         String arr[]=s.split(" ");
         System.out.println(Arrays.toString(arr));
     }
+}
