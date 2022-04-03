9d292586add607de6e60c7f5ba49969abb809224
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2022-04-03 10:56:07 +0430

    Create SevenClass.java

---
+++ src/SevenClass.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+
+        String s=" i liked ti move it move it";
+        String arr[]=s.split(" ");
+        System.out.println(Arrays.toString(arr));
+    }
